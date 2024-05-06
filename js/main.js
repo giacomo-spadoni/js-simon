a6af9a4516e8999ee2d68b94542366006f606960
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-06 03:18:48 +0200

    scusate ho dovuto farlo veloce e tardi quindi no commenti e molto basilare

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const section = document.getElementById('section')
 const numbers = []
+const playerNumbers = []
 
 for (let i = 0; i < 5; i++) {
     let number = Math.floor(Math.random() * 100) + 1
@@ -8,4 +9,22 @@ for (let i = 0; i < 5; i++) {
     newDiv.textContent = numbers[i]
     section.append(newDiv)
 }
+
+setTimeout(function () {
+    section.classList.add('hidden')
+}, 4 * 1000)
+
+setTimeout(function () {
+    for (let i = 0; i < 5; i++) {
+        let playerNumber = prompt('inserisci il tuo numero')
+        playerNumbers.push(playerNumber)
+    }
+    console.log(playerNumbers)
+    for (let i = 0; i < 5; i++) {
+        const element = parseInt(playerNumbers[i]); 43
+        if (numbers.includes(element)) {
+            alert('hai beccato il numero ' + element)
+        }
+    }
+}, 3 * 1000)
 
